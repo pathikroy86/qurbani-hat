7b5879b29712197b004a0df35b7ccf0d1573ad90
pathikroy86 <pathik.roy86@gmail.com>
2026-05-04 00:23:50 +0600

    create auth instance

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,9 +1,24 @@
+import Link from 'next/link';
 import React from 'react';
 
 const LoginPage = () => {
     return (
-        <div>
-            login
+        <div className='md:h-[600] flex justify-center items-center'>
+            <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input validator" placeholder="Email" required />
+                    <p className="validator-hint hidden">Required</p>
+                </fieldset>
+
+                <label className="fieldset">
+                    <span className="label">Password</span>
+                    <input type="password" className="input validator" placeholder="Password" required />
+                    <span className="validator-hint hidden">Required</span>
+                </label>
+                <button className="btn btn-neutral mt-4" type="submit">Login</button>
+                <p className='text-[#1F7A4D] text-center'>New here? <Link href="/signup" className='text-red-500 font-semibold'>Register</Link></p>
+            </form>
         </div>
     );
 };
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const RegisterPage = () => {
     return (
-        <div>
-            Register
+        <div className='md:h-[600] flex justify-center items-center'>
+            <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <fieldset className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="text" className="input" placeholder="Name" required />
+                    <p className="validator-hint hidden">Required</p>
+                </fieldset>
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input validator" placeholder="Email" required />
+                    <p className="validator-hint hidden">Required</p>
+                </fieldset>
+                <label className='fieldset'>
+                    <span className='label'>Image URL</span>
+                    <input type="url" className="input validator" required placeholder="https://" value="https://"
+                        pattern="^(https?://)?([a-zA-Z0-9]([a-zA-Z0-9-].*[a-zA-Z0-9])?.)+[a-zA-Z].*$"
+                        title="Must be valid URL" />
+                    <p className="validator-hint">Must be valid URL</p>
+                </label>
+                <label className="fieldset">
+                    <span className="label">Password</span>
+                    <input type="password" className="input validator" required placeholder="Password" minLength="8"
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
+                    <p className="validator-hint hidden">Required</p>
+                </label>
+                <button className="btn bg-[#1F7A4D] text-white mt-4" type="submit">Register</button>
+            </form>
         </div>
     );
 };
